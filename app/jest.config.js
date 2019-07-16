9930ee216dcf2bf9ea388eeee3ca882db9a2e9d3
Davide Scalzo <scalzodavide@yahoo.it>
2019-07-16 15:24:45 +0100

    separate commands for test and test:coverage

--- app/jest.config.js
+++ app/jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
     roots: ['<rootDir>/components', '<rootDir>/containers', '<rootDir>/lib'],
     setupFiles: ['./setupTests.js'],
     snapshotSerializers: ["enzyme-to-json/serializer"],
-    collectCoverage: true,
     collectCoverageFrom:[
         'components/**/*.{ts,tsx}',
         'containers/**/*.{ts,tsx}',
