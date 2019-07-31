7173b2d93a39e810dc1c4723be0fef0f4ea171e4
Davide Scalzo <scalzodavide@yahoo.it>
2019-07-31 16:45:48 +0100

    public-profile routes and fix top contributors

--- app/lib/getURLs.ts
+++ app/lib/getURLs.ts
@@ -1,16 +1,32 @@
 import slugify from 'slugify'
-interface IProps {
+interface IArticleProps {
     title: string
     id: string
     urlType?: string
 }
 
-export const getArticleURL = ({ title, id }: IProps) => ({
+export const getArticleURL = ({ title, id }: IArticleProps) => ({
     as: `${slugify(title, { lower: true })}/${id}/a`,
     href: `/article?article_id=${id}`,
 })
 
-export const getCollectionURL = ({ title, id }: IProps) => ({
-    as: `${slugify(title, { lower: true })}/${id}/c`,
-    href: `/collection?collection_id=${id}`,
+interface ICollectionProps {
+    name?: string | null
+    id?: string | null
+    urlType?: string
+}
+export const getCollectionURL = ({ name, id }: ICollectionProps) => ({
+    as: `${slugify(String(name), { lower: true })}/${String(id)}/c`,
+    href: `/collection?collection_id=${String(id)}`,
+})
+
+interface IProfileProps {
+    username?: string | null
+    userId?: string
+    urlType?: string
+}
+
+export const getProfileURL = ({ username, userId }: IProfileProps) => ({
+    as: `${slugify(String(username), { lower: true })}/${String(userId)}/p`,
+    href: `/public-profile?user_id=${String(userId)}`,
 })
