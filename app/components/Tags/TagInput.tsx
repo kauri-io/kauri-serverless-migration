c16743536e8f7d9cce0fe6cb9136c63eae3ae7c2
Davide <scalzodavide@yahoo.it>
2019-08-13 15:26:16 +0100

    fixed tag input

--- app/components/Tags/TagInput.tsx
+++ app/components/Tags/TagInput.tsx
@@ -3,6 +3,25 @@ import styled from 'styled-components'
 import Plus from './Plus'
 import { ITag } from './types'
 import { Component } from 'react'
+import { withStyles } from '@material-ui/styles'
+
+const styles = () => ({
+    input: {
+        width: '100%',
+        '&:hover': {
+            '& .MuiInput-underline::before': {
+                borderBottomColor: 'rgba(255,255,255,0.6)',
+            },
+        },
+        color: 'white',
+        '& .MuiInputBase-root': {
+            color: 'white',
+        },
+        '& .MuiInput-underline::before': {
+            borderBottomColor: 'rgba(255,255,255,0.3)',
+        },
+    },
+})
 
 interface IProps {
     availableTags?: ITag[]
@@ -11,6 +30,7 @@ interface IProps {
     onSelect?: (tag: ITag) => void
     handleEnterKey: (val: string) => void
     removeLastTag: () => void
+    classes: any
 }
 
 interface IState {
@@ -83,8 +103,6 @@ const Result = styled.div`
 `
 
 class TagInput extends Component<IProps, IState> {
-    private inputRef: any | null
-
     constructor(props: IProps) {
         super(props)
         this.state = {
@@ -94,13 +112,11 @@ class TagInput extends Component<IProps, IState> {
         }
         this.handleKey = this.handleKey.bind(this)
         this.handleClick = this.handleClick.bind(this)
-        this.inputRef = null
     }
 
     public handleClick(tag: ITag) {
         if (this.props.onSelect) {
             this.props.onSelect(tag)
-            this.inputRef.editValue('')
             ;(document.activeElement as HTMLElement).blur()
         }
         this.setState({ value: '', selectedIndex: 0 })
@@ -115,8 +131,6 @@ class TagInput extends Component<IProps, IState> {
                     ? this.props.availableTags[this.state.selectedIndex].tag
                     : e.currentTarget.value
             )
-            this.inputRef.value = ''
-            this.inputRef.editValue('')
             this.setState({ value: '', selectedIndex: 0 })
         }
         if (
@@ -143,10 +157,11 @@ class TagInput extends Component<IProps, IState> {
             const newIndex =
                 currentIndex <= 0 ? availableLength : (currentIndex -= 1)
             this.setState({ selectedIndex: newIndex })
-        } else {
-            console.log(e.currentTarget)
-            this.setState({ value: e.currentTarget.value })
         }
+    }
+
+    updateState(e: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({ value: e.target.value })
     }
 
     public render() {
@@ -173,6 +188,7 @@ class TagInput extends Component<IProps, IState> {
                     <TopRow>
                         <Plus />
                         <TextField
+                            className={this.props.classes.input}
                             onKeyPress={(
                                 e: React.KeyboardEvent<HTMLInputElement>
                             ) => {
@@ -180,10 +196,15 @@ class TagInput extends Component<IProps, IState> {
                                     e.preventDefault()
                                 }
                             }}
-                            ref={ref => (this.inputRef = ref)}
                             onKeyUp={this.handleKey}
-                            onChange={this.props.onChange}
+                            onChange={(
+                                e: React.ChangeEvent<HTMLInputElement>
+                            ) => {
+                                this.props.onChange && this.props.onChange(e)
+                                this.updateState(e)
+                            }}
                             placeholder="ADD TAG"
+                            value={this.state.value}
                         />
                     </TopRow>
                 </div>
@@ -228,4 +249,4 @@ class TagInput extends Component<IProps, IState> {
     }
 }
 
-export default TagInput
+export default withStyles(styles)(TagInput)
