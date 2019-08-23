ec7c5aff00539b82c2d8ab0effa0777cbc49f287
Davide <scalzodavide@yahoo.it>
2019-08-23 15:45:33 +0100

    add missing files

--- app/containers/Error/Error.test.tsx
+++ app/containers/Error/Error.test.tsx
@@ -3,7 +3,7 @@ import { mountWithTheme } from '../../setupTests'
 
 describe('containers/Error', () => {
     it('should match snapshot', () => {
-        const props = {}
+        const props = { code: '404' }
         const wrapper = mountWithTheme(<Error {...props} />)
         expect(wrapper).toMatchSnapshot()
     })
