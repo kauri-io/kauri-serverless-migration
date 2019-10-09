2a030dd3705d5efb0d5c68e6ec04b81d6f26c669
Davide <scalzodavide@yahoo.it>
2019-10-09 10:05:15 +0100

    tweak qa fixes

--- app/config/index.ts
+++ app/config/index.ts
@@ -1,5 +1,6 @@
 import production from './production'
 import development from './development'
+import qa from './qa'
 
 console.log('***** CONFIG: ', process.env.config)
-export default process.env.config === 'production' ? production : development
+export default process.env.config === 'production' ? production : process.env.config === 'qa' ? qa : development
