e82b7ac911098b6d231f492c4fdf2894468a621d
Davide Scalzo <scalzodavide@yahoo.it>
2019-10-24 14:54:59 +0100

    cleaned up yarn.lock (#86)
    * cleaned up yarn.lock
    
    * update nextconfig to latest bundle analyzer
    
    * fixed new bundle analyzer

--- app/next.config.js
+++ app/next.config.js
@@ -1,27 +1,10 @@
 const withCss = require("@zeit/next-css");
 const withPlugins = require("next-compose-plugins");
-const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.BUNDLE_ANALYZE === 'true',
+  })
 
-const plugins = [withCss]
-if (process.env.BUNDLE_ANALYZE) {
-    plugins.push([
-        withBundleAnalyzer,
-        {
-            analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
-            analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
-            bundleAnalyzerConfig: {
-                server: {
-                    analyzerMode: "static",
-                    reportFilename: "./bundles/server.html",
-                },
-                browser: {
-                    analyzerMode: "static",
-                    reportFilename: "./bundles/client.html",
-                },
-            },
-        },
-    ]);
-}
+const plugins = [withCss, withBundleAnalyzer]
 
 const nextConfig = {
     env: {
